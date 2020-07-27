07573ffd10b8da93bfa631857a6f7e6e26a83842
Salvador <chava_r17@hotmail.cm>
2020-07-27 00:45:11 -0500

    Modificacion MiTiendita Api
    -Se agregaron nuevas rutas.

--- mitiendita/Api/main.py
+++ mitiendita/Api/main.py
@@ -1,9 +1,10 @@
 from flask import Flask
 import pypyodbc
+from flask_cors import CORS
 cadenaConexion = 'Driver={SQL Server};Server=nerus.sytes.net\SQL2017;Database=pdv;uid=usr_arquos;pwd=usr_arquos;'
 
 app = Flask(__name__)
-
+CORS(app)
 @app.route('/')
 def defautl():
     return "MiTiendita API, Version 1.1.0"
@@ -13,17 +14,37 @@ def productos():
 
     conexion = pypyodbc.connect(cadenaConexion)
     cursor = conexion.cursor()
-    query = "SELECT id_producto, descripcion FROM dbo.Opr_productos "
+    query = "SELECT id_producto, descripcion, existencia, precio_v FROM dbo.Opr_productos "
 
     cursor.execute(query)
 
+    tmpUrlImage="https://www.freeiconspng.com/uploads/no-image-icon-10.png"
     result = []
     while True:
         row = cursor.fetchone()
         if not row:
             break
-        # xr = {'Id':row['id_producto'],'Descripcion':row['descripcion'],'Existencia':row['existencia'],'Precio':row['precio_v']}
-        xr = {'Id':row['id_producto'], 'Descripcion':row['descripcion']}
+        xr = { 'Id':str(row['id_producto']), 'Descripcion':str(row['descripcion']), 'Existencia':str(row['existencia']), 'Precio':str(row['precio_v']), 'ImagenUrl':tmpUrlImage}
+        result.append(xr)
+    cursor.close()
+    conexion.close()
+
+    return {'productos':result}
+    
+@app.route('/busqueda/<palabra>')
+def show_user_profile(palabra):
+    conexion = pypyodbc.connect(cadenaConexion)
+    cursor = conexion.cursor()
+    query = "SELECT id_producto, descripcion, existencia, precio_v FROM dbo.Opr_productos WHERE DESCRIPCION LIKE '%" + palabra + "%'"
+    cursor.execute(query)
+    
+    tmpUrlImage="https://www.freeiconspng.com/uploads/no-image-icon-10.png"
+    result = []
+    while True:
+        row = cursor.fetchone()
+        if not row:
+            break
+        xr = { 'Id':str(row['id_producto']), 'Descripcion':str(row['descripcion']), 'Existencia':str(row['existencia']), 'Precio':str(row['precio_v']), 'ImagenUrl':tmpUrlImage}
         result.append(xr)
     cursor.close()
     conexion.close()
